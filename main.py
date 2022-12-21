913d6190b4ef0bbae11043a60f1e44617b447bfb
Grimmys <funkyzermmy@gmail.com>
2022-12-21 19:40:24 +0100

    Add settings command and possibility to select language

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from discord import Intents, Client, app_commands, Embed
+import discord
+from discord import Intents, Client, app_commands, Embed, SelectOption
 import logging
-from pokemontcgsdk import Card
+
+from discord.ui import Select, View
+from pokemontcgsdk import Card, PokemonTcgException
 
 from os import environ as env
 
@@ -12,19 +15,49 @@ tree = app_commands.CommandTree(client)
 
 
 @tree.command(name="ping", description="Get bot latency")
-async def ping_command(interaction):
+async def ping_command(interaction: discord.Interaction):
     await interaction.response.send_message(f"My ping is** {round(client.latency * 1000)}ms**")
 
-    
+
 @tree.command(name="card", description="Get a card with its id")
-async def card(interaction, id: str):
+async def get_card(interaction: discord.Interaction, card_id: str):
     try:
-        card = Card.find(id)
-        embed = Embed(title=card.name, description=id, color=0x00ff00)
+        card = Card.find(card_id)
+        embed = Embed(title=card.name, description=card_id, color=0x00ff00)
         embed.set_image(url=card.images.large if card.images.large else card.images.small)
         await interaction.response.send_message(embed=embed)
-    except:
-        await interaction.response.send_message(f'Card {id} does not exist.')
+    except PokemonTcgException:
+        await interaction.response.send_message(f'Card {card_id} does not exist.')
+
+
+language_options = [
+    SelectOption(label="Français", emoji="🇫🇷", value="0", description="Changer la langue en français"),
+    SelectOption(label="English", emoji="🇬🇧", value="1", description="Switch to English"),
+]
+
+
+@tree.command(name="settings", description="Change user settings")
+async def settings(interaction: discord.Interaction):
+    embed = Embed(title="---------- Settings ----------", color=0x808080)
+    embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+    embed.add_field(name="Language", value=":flag_gb: English",
+                    inline=False)
+
+    async def change_language_callback(language_interaction):
+        selected_index = int(select.values[0])
+        await language_interaction.response.send_message(
+            f"Language changed to: {language_options[selected_index].label}", delete_after=2)
+
+    select = Select(
+        placeholder="Change language",
+        options=language_options
+    )
+    select.callback = change_language_callback
+
+    view = View()
+    view.add_item(select)
+
+    await interaction.response.send_message(embed=embed, view=view)
 
 
 @client.event
