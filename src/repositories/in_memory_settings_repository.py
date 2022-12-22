8de50aa54a981207bd610d36b41c0fc32e97814b
Grimmys <funkyzermmy@gmail.com>
2022-12-22 17:52:30 +0100

    Change data structure used for in memory repository from list to dict

--- src/repositories/in_memory_settings_repository.py
+++ src/repositories/in_memory_settings_repository.py
@@ -6,25 +6,19 @@ from src.entities.user_settings_entity import UserSettingsEntity
 
 class InMemorySettingsRepository(SettingsRepository):
     def __init__(self):
-        self._user_settings_entities: list[UserSettingsEntity] = []
+        self._user_settings_entities_by_id: dict[int, UserSettingsEntity] = {}
 
     def get_user_settings(self, user_id: int) -> Optional[UserSettingsEntity]:
-        for saved_entity in self._user_settings_entities:
-            if saved_entity.user_id == user_id:
-                return saved_entity
+        if user_id in self._user_settings_entities_by_id:
+            return self._user_settings_entities_by_id[user_id]
         return None
 
     def save_user_settings(self, user_settings: UserSettingsEntity) -> bool:
-        for index, saved_entity in enumerate(self._user_settings_entities):
-            if saved_entity.user_id == user_settings.user_id:
-                self._user_settings_entities[index] = user_settings
-                return True
-        self._user_settings_entities.append(user_settings)
+        self._user_settings_entities_by_id[user_settings.user_id] = user_settings
         return True
 
     def change_user_language(self, user_id: int, new_language_id: int) -> bool:
-        for saved_entity in self._user_settings_entities:
-            if saved_entity.user_id == user_id:
-                saved_entity.language_id = new_language_id
-                return True
+        if user_id in self._user_settings_entities_by_id:
+            self._user_settings_entities_by_id[user_id].language_id = new_language_id
+            return True
         return False
