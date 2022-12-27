a1df5f554a7ac6fa2a3feb6c1551324298eaa378
neufDePique <noneedforanameoraface@gmail.com>
2022-12-27 01:41:42 +0100

    Add search pagination for text search
    -Refactor search result to explicitly fetch all result at once and filter in-app
    -Add PaginatedEmbed class for easy reusability of the pagination mechanics

--- src/commands/search_command.py
+++ src/commands/search_command.py
@@ -1,19 +1,39 @@
 import discord
-from discord import app_commands, Embed
+from discord import app_commands
 from discord.ext import commands
+from discord.ui import View, Button
 from pokemontcgsdk import Card
 
-SEARCH_PAGE_SIZE=10
+from src.components.paginated_embed import PaginatedEmbed
+
+SEARCH_PAGE_SIZE = 10
+
 
 class SearchCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-
     @app_commands.command(name="search", description="Search card with several parameters")
     async def search_command(self, interaction: discord.Interaction, content: str) -> None:
-        cards = Card.where(q=f"name:*{content}*", page=1, pagesize=SEARCH_PAGE_SIZE)
-        embed = Embed()
-        for card in cards:
-            embed.add_field(name=card.id, value=card.name, inline=False)
-        await interaction.response.send_message(embed=embed)
+        all_cards = [{"name": card.id, "value": card.name}
+                     for card in Card.where(q=f"name:*{content}*")]
+        embed = PaginatedEmbed(all_cards, SEARCH_PAGE_SIZE)
+        view = View()
+
+        async def change_page_callback(click_interaction: discord.Interaction, forward):
+            embed.change_page(forward)
+            await interaction.edit_original_response(embed=embed.embed)
+            await click_interaction.response.defer()
+
+        next_button = Button(emoji="➡️")
+        next_button.callback = lambda click_interaction: change_page_callback(
+            click_interaction, True)
+
+        previous_button = Button(emoji="⬅️")
+        previous_button.callback = lambda click_interaction: change_page_callback(
+            click_interaction, False)
+
+        view.add_item(previous_button)
+        view.add_item(next_button)
+
+        await interaction.response.send_message(embed=embed.embed, view=view)
