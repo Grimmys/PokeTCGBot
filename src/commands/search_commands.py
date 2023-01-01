ccc91ab599bd38047f7322edecd1d9cd858b07e1
Grimmys <funkyzermmy@gmail.com>
2023-01-01 15:08:16 +0100

    Change name of argument for fetching cards with images

--- src/commands/search_commands.py
+++ src/commands/search_commands.py
@@ -1,4 +1,5 @@
 import pickle
+from typing import Literal
 
 import discord
 from discord import app_commands
@@ -31,7 +32,7 @@ class SearchCog(commands.Cog):
         return {card.id: card for card in cards}
 
     @app_commands.command(name="search", description="Search card with several parameters")
-    async def search_command(self, interaction: discord.Interaction, content: str, image_mode: bool = False) -> None:
+    async def search_command(self, interaction: discord.Interaction, content: str, with_image: bool = False) -> None:
         user_language_id = self.settings_service.get_user_language_id(
             interaction.user.id)
         all_cards = [{"name": card.id, "value": card.name,
@@ -43,7 +44,7 @@ class SearchCog(commands.Cog):
                 self.t(user_language_id, 'search_cmd.not_found').replace("{1}", content))
             return
 
-        embed = PaginatedEmbed(all_cards, image_mode, 1 if image_mode else SEARCH_PAGE_SIZE)
+        embed = PaginatedEmbed(all_cards, with_image, 1 if with_image else SEARCH_PAGE_SIZE)
         view = View()
 
         async def change_page_callback(click_interaction: discord.Interaction, forward):
@@ -65,7 +66,7 @@ class SearchCog(commands.Cog):
         await interaction.response.send_message(embed=embed.embed, view=view)
 
     @app_commands.command(name="collection", description="Search cards in your own collection")
-    async def collection_command(self, interaction: discord.Interaction, image_mode: bool = False) -> None:
+    async def collection_command(self, interaction: discord.Interaction, with_image: bool = False) -> None:
         user = self.user_service.get_user(interaction.user.id)
         user_language_id = user.settings.language_id
 
@@ -77,7 +78,7 @@ class SearchCog(commands.Cog):
             }
             formatted_id = f"**ID**: {card_id}"
             formatted_quantity = f"**{self.t(user_language_id, 'common.quantity').capitalize()}**: {quantity}"
-            if image_mode:
+            if with_image:
                 formatted_rarity = f"**{self.t(user_language_id, 'common.rarity').capitalize()}**: {card.rarity}"
                 entry_card["value"] = f"{formatted_id}\n{formatted_rarity}\n{formatted_quantity}"
                 entry_card["image"] = card.images.large if card.images.large else card.images.small
@@ -90,7 +91,7 @@ class SearchCog(commands.Cog):
                 self.t(user_language_id, 'collection_cmd.empty'))
             return
 
-        embed = PaginatedEmbed(own_cards, image_mode, 1 if image_mode else SEARCH_PAGE_SIZE)
+        embed = PaginatedEmbed(own_cards, with_image, 1 if with_image else SEARCH_PAGE_SIZE)
         view = View()
 
         async def change_page_callback(click_interaction: discord.Interaction, forward):
