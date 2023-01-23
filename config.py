b29731502deb22b19dc9230ad26ddde6ef3d28f8
Grimmys <funkyzermmy@gmail.com>
2023-01-23 20:14:45 +0100

    Add more variables configs for gameplay balancing things

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
+from typing import List
+
 # Alternative ways of sending required arguments to the application if env variables is not an option for your
 # deployment
-from typing import List
 
 DISCORD_TOKEN = None
 
 # Required config parameters to replace
 
+# -- Gameplay balancing related
 UNCOMMON_UPGRADE_RATE: float = None
+DEFAULT_BASIC_BOOSTER_COOLDOWN: int = None
+DEFAULT_PROMO_BOOSTER_COOLDOWN: int = None
+DAILY_MONEY_GIFT_AMOUNT: int = None
+BOOSTERS_PRICE: dict[str, int] = {
+    "Basic": None,
+    "Promo": None
+}
+
+# -- Discord related
 LOG_CHANNEL_ID: int = None
 BOT_ADMIN_USER_IDS: List[int] = []
