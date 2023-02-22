fd091a51c076c47b122725ee91e924c20b74a396
Grimmys <funkyzermmy@gmail.com>
2023-02-22 19:38:01 +0100

    Add graded cards to user entity

--- src/entities/user_entity.py
+++ src/entities/user_entity.py
@@ -2,12 +2,14 @@ import time
 
 from src.entities.user_cooldowns_entity import UserCooldownsEntity
 from src.entities.user_settings_entity import UserSettingsEntity
+from src.utils.card_grade import CardGrade
 
 
 class UserEntity:
-    def __init__(self, user_id: int, name_tag: str = "", last_interaction_date: int = int(time.time()), money: int = 0, 
+    def __init__(self, user_id: int, name_tag: str = "", last_interaction_date: int = int(time.time()), money: int = 0,
                  boosters_quantity: int = 0, promo_boosters_quantity: int = 0,
-                 cards_by_id: dict[str, int] = None, user_settings_entity: UserSettingsEntity = None,
+                 cards_by_id: dict[str, int] = None, graded_cards: dict[tuple[str, CardGrade], int] = None,
+                 user_settings_entity: UserSettingsEntity = None,
                  user_cooldowns_entity=None):
         self.id: int = user_id
         self.name_tag: str = name_tag
@@ -16,6 +18,7 @@ class UserEntity:
         self.boosters_quantity: int = boosters_quantity
         self.promo_boosters_quantity: int = promo_boosters_quantity
         self.cards: dict[str, int] = cards_by_id if cards_by_id is not None else {}
+        self.graded_cards: dict[tuple[str, CardGrade], int] = graded_cards if graded_cards is not None else {}
         self.settings = user_settings_entity if user_settings_entity is not None else UserSettingsEntity()
         self.cooldowns = user_cooldowns_entity if user_cooldowns_entity is not None else UserCooldownsEntity()
 
@@ -27,5 +30,6 @@ class UserEntity:
         self.boosters_quantity = state.get("boosters_quantity", 0)
         self.promo_boosters_quantity = state.get("promo_boosters_quantity", 0)
         self.cards = state.get("cards", {})
+        self.graded_cards = state.get("graded_cards", {})
         self.settings = state.get("settings", UserSettingsEntity())
         self.cooldowns = state.get("cooldowns", UserCooldownsEntity())
