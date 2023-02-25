77db0cd536726ad591694884e52552788d1bc4a3
Haythem <haythem42@outlook.fr>
2023-02-25 15:43:11 +0100

    File config.py updated

--- config.py
+++ config.py
@@ -3,21 +3,20 @@ from typing import List
 # Alternative ways of sending required arguments to the application if env variables is not an option for your
 # deployment
 
-DISCORD_TOKEN = "MTA1NDYzNjk1NzI5ODMzMTcwOQ.GV9OD3.m4uIMAc8uNhYcP27lpNmz8yH9FYNarHb2u4MOw"
+DISCORD_TOKEN = None
 
 # Required config parameters to replace
 
 # -- Gameplay balancing related
-UNCOMMON_UPGRADE_RATE: float = None
-DEFAULT_BASIC_BOOSTER_COOLDOWN: int = None
-DEFAULT_PROMO_BOOSTER_COOLDOWN: int = None
-DEFAULT_GRADING_COOLDOWN: int = None
-DAILY_MONEY_GIFT_AMOUNT: int = None
+UNCOMMON_UPGRADE_RATE: float = 4.5
+DEFAULT_BASIC_BOOSTER_COOLDOWN: int = 10
+DEFAULT_PROMO_BOOSTER_COOLDOWN: int = 15
+DEFAULT_GRADING_COOLDOWN: int = 40
+DAILY_MONEY_GIFT_AMOUNT: int = 5000
 BOOSTERS_PRICE: dict[str, int] = {
-    "Basic": None,
-    "Promo": None
+    "Basic": 100,
+    "Promo": 100
 }
-
 # -- Discord related
-LOG_CHANNEL_ID: int = None
-BOT_ADMIN_USER_IDS: List[int] = [381860176677961730]
+LOG_CHANNEL_ID: int = 0.01
+BOT_ADMIN_USER_IDS: List[int] = []
