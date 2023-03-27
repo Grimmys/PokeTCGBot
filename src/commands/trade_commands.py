2ae86651921046b136e898e2fcdfc4449b15c0cc
Grimmys <funkyzermmy@gmail.com>
2023-03-27 21:37:57 +0200

    Base of UI part for secure trade command

--- src/commands/trade_commands.py
+++ src/commands/trade_commands.py
@@ -1,11 +1,14 @@
 import discord
-from discord import app_commands
-from discord.ext import commands
+from discord import app_commands, Embed
 from discord.app_commands import locale_str as _T
+from discord.ext import commands
+from discord.ui import View, Button
 
 from config import LOG_CHANNEL_ID
+from src.colors import GREEN
 from src.services.localization_service import LocalizationService
 from src.services.user_service import UserService
+from src.utils.flags import is_dev_mode
 
 
 class TradingCog(commands.Cog):
@@ -33,8 +36,6 @@ class TradingCog(commands.Cog):
 
         other_user = self.user_service.get_user(member)
 
-        card_ids_list: list[str] = card_ids.split()
-
         if other_user is None:
             await interaction.response.send_message(self._t(user_language_id, 'common.user_not_found'))
             return
@@ -43,6 +44,7 @@ class TradingCog(commands.Cog):
             await interaction.response.send_message(self._t(user_language_id, 'send_cards_cmd.same_user'))
             return
 
+        card_ids_list: list[str] = card_ids.split()
         success_transfer = self.user_service.transfer_cards(user.id, other_user.id, card_ids_list)
         if not success_transfer:
             await interaction.response.send_message(self._t(user_language_id, 'send_cards_cmd.missing_cards'))
@@ -81,3 +83,59 @@ class TradingCog(commands.Cog):
             f"{user.id} ({user.name_tag}) sent {amount} Pokémon Dollars to {other_user.id} ({other_user.name_tag})")
         await interaction.response.send_message(self._t(user_language_id, 'send_money_cmd.money_transferred')
                                                 .format(user=other_user.name_tag, amount=amount))
+
+    @app_commands.command(name=_T("secured_trade_cmd-name"), description=_T("secured_trade_cmd-desc"))
+    async def secured_trade_command(self, interaction: discord.Interaction, member: discord.User, own_card_ids: str,
+                                    other_player_card_ids: str) -> None:
+        user = self.user_service.get_and_update_user(interaction.user, interaction.locale)
+        user_language_id = user.settings.language_id
+
+        if not is_dev_mode():
+            await interaction.response.send_message(self._t(user_language_id, 'common.feature_disabled'))
+            return
+
+        if user.is_banned:
+            await interaction.response.send_message(self._t(user_language_id, 'common.user_banned'))
+            return
+
+        other_user = self.user_service.get_user(member)
+
+        if other_user is None:
+            await interaction.response.send_message(self._t(user_language_id, 'common.user_not_found'))
+            return
+
+        if user.id == other_user.id:
+            await interaction.response.send_message(self._t(user_language_id, 'secured_trade_cmd.same_user'))
+            return
+
+        own_card_ids_split: list[str] = own_card_ids.split()
+        other_player_card_ids_split: list[str] = other_player_card_ids.split()
+
+        embed = Embed(
+            title=f"---------- {self._t(user_language_id, 'secured_trade_cmd.title')} ----------",
+            color=GREEN)
+        embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+
+        embed.add_field(name=f"{user.name_tag}:", value=", ".join(own_card_ids_split), inline=False)
+        embed.add_field(name=f"{other_user.name_tag}:", value=", ".join(other_player_card_ids_split), inline=False)
+
+        async def validate_trade(confirm_interaction: discord.Interaction):
+            if confirm_interaction.user.id != other_user.id:
+                await confirm_interaction.response.send_message(
+                    self._t(user_language_id, 'secured_trade_cmd.wrong_confirmation_user').format(user=other_user.name_tag),
+                    delete_after=2
+                )
+
+            await confirm_interaction.response.send_message(
+                self._t(user_language_id, 'secured_trade_cmd.trade_confirmed_response_msg'),
+                delete_after=2
+            )
+
+        view = View()
+        confirm_button = Button(
+            label=self._t(user_language_id, 'secured_trade_cmd.confirmation_label'),
+            style=discord.ButtonStyle.blurple)
+        confirm_button.callback = validate_trade
+        view.add_item(confirm_button)
+
+        await interaction.response.send_message(embed=embed, view=view)
